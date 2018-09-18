099530934c7d1a242929f305ba8ada8c97670dbc
quangle3 <quangldd00747@fpt.edu.vn>
2018-09-18 12:03:24 +0700

    quang update

---
+++ src/Program.java
@@ -0,0 +1,9 @@
+import game.GameWindow;
+
+public class Program {
+
+    public static void main(String[] args) {
+        GameWindow gameWindow = new GameWindow();
+        gameWindow.mainLoop();
+    }
+}
---
+++ src/game/GameWindow.java
@@ -0,0 +1,56 @@
+package game;
+
+import inputs.InputManager;
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class GameWindow extends JFrame {
+    
+    GameCanvas gameCanvas;
+
+    public GameWindow() {
+
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent windowEvent) {
+                System.exit(0);
+            }
+        });
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                InputManager.instance.keyPressed(keyEvent);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+                InputManager.instance.keyReleased(keyEvent);
+            }
+        });
+        // change later
+        this.setSize(600,800);
+        this.setResizable(false);
+        this.setTitle("The-Resue-Doctor");
+
+        gameCanvas = new GameCanvas();
+        this.setContentPane(gameCanvas);
+        this.setVisible(true);
+    }
+
+    public void mainLoop() {
+        long currentTime = 0;
+        long lastTimeRender = 0;
+        while (true) {
+            currentTime = System.nanoTime();
+            if (currentTime - lastTimeRender >= 17_000_000) {
+                gameCanvas.run();
+                gameCanvas.render();
+                lastTimeRender = currentTime;
+            }
+        }
+    }
+}
