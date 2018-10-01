acf4e454fdfb433ea91ebdca393f1e15bea29419
quangle3 <quangldd00747@fpt.edu.vn>
2018-10-01 19:35:05 +0700

    ad

--- src/buff/AddBlood.java
+++ src/buff/AddBlood.java
@@ -10,31 +10,31 @@ public class AddBlood extends GameObject {
     private int count;
 
     public AddBlood(int x, int y) {
-        super(x,y);
+        super(x, y);
         this.renderer = new Animation(
                 ImageUtil.LoadImage("images/bullets/bullet.png"),
                 ImageUtil.LoadImage("images/bullets/bullet4.png"),
                 ImageUtil.LoadImage("images/bullets/bullet.png")
         );
-        this.boxCollider = new BoxCollider(20,20);
-        this.check = true;
+        this.boxCollider = new BoxCollider(20, 20);
         this.count = 0;
     }
 
     @Override
     public void run() {
         super.run();
-        if (check) {
+        if (!check) {
             count++;
             position.y -= 3;
-            if (count >= 70) {
-                check = false;
+            if (count == 70) {
+                check = true;
             }
-        } else {
+        }
+        if (check) {
             count--;
             position.y += 3;
             if (count == 0) {
-                check = true;
+                check = false;
             }
         }
     }
--- src/buff/AddBullet.java
+++ src/buff/AddBullet.java
@@ -4,7 +4,7 @@ import bases.*;
 
 public class AddBullet extends GameObject {
     private int count;
-    private static boolean check;
+    private boolean check;
     public AddBullet(int x, int y) {
         super(x,y);
         this.renderer = new Animation(
@@ -13,24 +13,24 @@ public class AddBullet extends GameObject {
                 ImageUtil.LoadImage("images/bullets/bullet3.png")
         );
         count = 0;
-        check = true;
         this.boxCollider = new BoxCollider(20,20);
     }
 
     @Override
     public void run() {
         super.run();
-        if (check) {
+        if (!check) {
             count++;
-            position.addUp(0,-3);
-            if (count >= 70) {
-                check = false;
-            }
-        } else {
-            count--;
-            position.addUp(0,3);
-            if (count == 0) {
+            position.y -= 3;
+            if (count == 70) {
                 check = true;
+            }
+        }
+        if (check) {
+            count--;
+            position.y += 3;
+            if (count == 0) {
+                check = false;
             }
         }
     }
